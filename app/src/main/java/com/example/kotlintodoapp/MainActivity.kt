2c1cb4b538299629bd26fc5d96123915a5fd0b97
Touhid <touhidul.islam.official@gmail.com>
2023-07-12 14:06:54 +0600

    Initial commit -  Splash Screen

---
+++ app/src/main/java/com/example/kotlintodoapp/MainActivity.kt
@@ -0,0 +1,22 @@
+package com.example.kotlintodoapp
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+
+private const val SPLASH_SCREEN_DELAY: Long = 5000 //value is in milliseconds
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        Handler(Looper.getMainLooper()).postDelayed({
+            val intent = Intent(this, HomeActivity::class.java)
+            startActivity(intent)
+            finish()
+        }, SPLASH_SCREEN_DELAY)
+    }
+}
